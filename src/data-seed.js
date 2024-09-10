2505f560ac92787d3ca1a7419313a6a3e640d956
Abdurrahman Abdul Hamid <74585156+Artcods@users.noreply.github.com>
2024-09-10 17:47:43 +0700

    Create Object Data-Seed.js and Import to IndexProduct.vue

---
+++ src/data-seed.js
@@ -0,0 +1,188 @@
+import productImage1 from '../src/assets/Photo 1.jpg';
+import productImage2 from '../src/assets/Photo 2.jpg';
+import productImage3 from '../src/assets/Photo 3.jpg';
+import productImage4 from '../src/assets/Photo 4.jpg';
+import productImage5 from '../src/assets/Photo 5.jpg';
+import productImage6 from '../src/assets/Photo 6.jpg';
+import productImage7 from '../src/assets/Photo 7.jpg';
+import productImage8 from '../src/assets/Photo 8.jpg';
+import productImage9 from '../src/assets/Photo 9.jpg';
+import productImage10 from '../src/assets/Photo 10.jpg';
+import productImage11 from '../src/assets/Photo 11.jpg';
+import productImage12 from '../src/assets/Photo 12.jpg';
+import productImage13 from '../src/assets/Photo 13.jpg';
+import productImage14 from '../src/assets/Photo 14.jpg';
+import productImage15 from '../src/assets/Photo 15.jpg';
+import productImage16 from '../src/assets/Photo 16.jpg';
+import productImage17 from '../src/assets/Photo 17.jpg';
+import productImage18 from '../src/assets/Photo 18.jpg';
+import productImage19 from '../src/assets/Photo 19.jpg';
+import productImage20 from '../src/assets/Photo 20.jpg';
+
+export const products = [{
+    id: 1,
+    name: 'Product 1',
+    price: 100000,
+    description: 'Product 1 Description',
+    image: productImage1,
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 200000,
+    description: 'Product 2 Description',
+    image: productImage2,
+    quantity: 5
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 300000,
+    description: 'Product 3 Description',
+    image: productImage3,
+    quantity: 3
+  },
+  {
+    id: 4,
+    name: 'Product 4',
+    price: 400000,
+    description: 'Product 4 Description',
+    image: productImage4,
+    quantity: 15
+  },
+  {
+    id: 5,
+    name: 'Product 5',
+    price: 500000,
+    description: 'Product 5 Description',
+    image: productImage5,
+    quantity: 7
+  },
+  {
+    id: 6,
+    name: 'Product 6',
+    price: 600000,
+    description: 'Product 6 Description',
+    image: productImage6,
+    quantity: 12
+  },
+  {
+    id: 7,
+    name: 'Product 7',
+    price: 700000,
+    description: 'Product 7 Description',
+    image: productImage7,
+    quantity: 8
+  },
+  {
+    id: 8,
+    name: 'Product 8',
+    price: 800000,
+    description: 'Product 8 Description',
+    image: productImage8,
+    quantity: 13
+  },
+  {
+    id: 9,
+    name: 'Product 9',
+    price: 900000,
+    description: 'Product 9 Description',
+    image: productImage9,
+    quantity: 6
+  },
+  {
+    id: 10,
+    name: 'Product 10',
+    price: 1000000,
+    description: 'Product 10 Description',
+    image: productImage10,
+    quantity: 4
+  },
+  {
+    id: 11,
+    name: 'Product 11',
+    price: 1100000,
+    description: 'Product 11 Description',
+    image: productImage11,
+    quantity: 9
+  },
+  {
+    id: 12,
+    name: 'Product 12',
+    price: 1200000,
+    description: 'Product 12 Description',
+    image: productImage12,
+    quantity: 11
+  },
+  {
+    id: 13,
+    name: 'Product 13',
+    price: 1300000,
+    description: 'Product 13 Description',
+    image: productImage13,
+    quantity: 14
+  },
+  {
+    id: 14,
+    name: 'Product 14',
+    price: 1400000,
+    description: 'Product 14 Description',
+    image: productImage14,
+    quantity: 16
+  },
+  {
+    id: 15,
+    name: 'Product 15',
+    price: 1500000,
+    description: 'Product 15 Description',
+    image: productImage15,
+    quantity: 20
+  },
+  {
+    id: 16,
+    name: 'Product 16',
+    price: 1600000,
+    description: 'Product 16 Description',
+    image: productImage16,
+    quantity: 18
+  },
+  {
+    id: 17,
+    name: 'Product 17',
+    price: 1700000,
+    description: 'Product 17 Description',
+    image: productImage17,
+    quantity: 19
+  },
+  {
+    id: 18,
+    name: 'Product 18',
+    price: 1800000,
+    description: 'Product 18 Description',
+    image: productImage18,
+    quantity: 2
+  },
+  {
+    id: 19,
+    name: 'Product 19',
+    price: 1900000,
+    description: 'Product 19 Description',
+    image: productImage19,
+    quantity: 17
+  },
+  {
+    id: 20,
+    name: 'Product 20',
+    price: 2000000,
+    description: 'Product 20 Description',
+    image: productImage20,
+    quantity: 10
+  }
+]
+
+export const cartItems = [
+    products[0],
+    products[1],
+    products[2]
+]
--- src/views/product/IndexProduct.vue
+++ src/views/product/IndexProduct.vue
@@ -3,15 +3,67 @@
 <template>
   <div>
     <h1>Home Product</h1>
+
+    <div class="grid-warp">
+      <!-- Menampilkan semua produk -->
+      <div v-for="product in products" :key="product.id" class="product-item">
+        <h2 class="product-name">{{ product.name }}</h2>
+        <p class="product-price">${{ product.price }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
 
+/* Mengambil data product dari file seed */
+import { products } from '../../data-seed'
+
+/* Mengenalkannya pada data di export default */
+export default {
+  data() {
+    return {
+      products
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+  .grid-warp {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
 
+  .product-item {
+    align-items: center;
+    border-radius: 8px;
+    box-shadow: 0px 2px 5px #888;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2%;
+    padding: 0px;
+    position: relative;
+    width: 32%;
+  }
+
+  .product-name {
+    margin-bottom: 0;
+  }
+
+  img {
+    height: 100px;
+    width: 200px;
+    border-radius: 5px;
+  }
+
+  a {
+    width: 100%;
+  }
+
+  button {
+    width: 100%;
+  }
 </style>
