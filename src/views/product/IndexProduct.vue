fb79b45654738f2822fb4d27098a667e2c88bcae
Abdurrahman Abdul Hamid <74585156+Artcods@users.noreply.github.com>
2024-09-11 14:39:59 +0700

    Membuat Components Untuk Digunakan oleh IndexProduct.vue

--- src/views/product/IndexProduct.vue
+++ src/views/product/IndexProduct.vue
@@ -4,17 +4,13 @@
   <div>
     <h1>Home Product</h1>
 
-    <div class="grid-warp">
+    <div class="page-wrap">
       <!-- Menampilkan semua produk -->
-      <div v-for="product in products" :key="product.id" class="product-item">
-      <img :src="product.image" alt="">
-        <h2 class="product-name">{{ product.name }}</h2>
-        <p class="product-price">Rp.{{ product.price }}</p>
-
-        <!-- Membuat Link untuk button menuju ke detail (menambahkan property params yang berisikan id) -->
-        <router-link :to="{ name: 'detail', params :{id: product.id} }">
-          <button>Detail Page</button>
-        </router-link>
+      <div class="grid-wrap">
+        <!-- buat props lalu diisi dengan data products -->
+         <!--  Membuat props di componentsnya untuk mengirimkan data dari product ke components -->
+        <ProductItemComponent v-for="product in products" :key="product.id" 
+        :product="product"/>
       </div>
     </div>
   </div>
@@ -24,53 +20,26 @@
 
 /* Mengambil data product dari file seed */
 import { products } from '../../data-seed'
+import ProductItemComponent from '../../components/ProductItem.vue'
 
 /* Mengenalkannya pada data di export default */
 export default {
   data() {
     return {
-      products
+      products,
     }
+  },
+  components: {
+    ProductItemComponent
   }
 }
 </script>
 
 <style scoped>
-  .grid-warp {
+  .grid-wrap {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
-    margin-top: 20px;
-  }
-
-  .product-item {
-    align-items: center;
-    border-radius: 8px;
-    box-shadow: 0px 2px 5px #888;
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 2%;
-    padding: 20px;
-    position: relative;
-    width: 32%;
-  }
-
-  .product-name {
-    margin-bottom: 0;
-  }
-
-  img {
-    height: 140px;
-    width: 200px;
-    border-radius: 5px;
-  }
-
-  a {
-    width: 100%;
-  }
-
-  button {
-    align-items: center;
-    width: 50%;
+    margin-top: 16px;
   }
 </style>
